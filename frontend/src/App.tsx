7c7b06838fe80b135675dad7c5377a39721965b6
Chris Hull <chris.f.hull@gmail.com>
2023-08-29 14:21:24 -0400

    hide code from ui

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,7 @@ import "./App.css";
 import Input from "./components/Input";
 import Sidebar from "./components/Sidebar";
 import Chat, { WaitingStates } from "./components/Chat";
-import React, { useState, useEffect  } from "react";
+import React, { useState, useEffect } from "react";
 import Config from "./config";
 import { useLocalStorage } from "usehooks-ts";
 
@@ -15,7 +15,9 @@ export type MessageDict = {
 function App() {
   const COMMANDS = ["reset"];
 
-  let [MODELS, setModels] = useState([{displayName: "GPT-3.5", name: "gpt-3.5-turbo"}]);
+  const [MODELS, setModels] = useState([
+    { displayName: "GPT-3.5", name: "gpt-3.5-turbo" },
+  ]);
 
   useEffect(() => {
     const getModels = async () => {
@@ -25,20 +27,20 @@ function App() {
         setModels(json);
       } catch (e) {
         console.error(e);
-      };
+      }
     };
 
     getModels();
- }, []);
+  }, []);
 
-  let [selectedModel, setSelectedModel] = useLocalStorage<string>(
+  const [selectedModel, setSelectedModel] = useLocalStorage<string>(
     "model",
     MODELS[0].name
   );
 
-  let [openAIKey, setOpenAIKey] = useLocalStorage<string>("OpenAIKey", "");
+  const [openAIKey, setOpenAIKey] = useLocalStorage<string>("OpenAIKey", "");
 
-  let [messages, setMessages] = useState<Array<MessageDict>>(
+  const [messages, setMessages] = useState<Array<MessageDict>>(
     Array.from([
       {
         text: "Hello! I'm a GPT Code assistant. Ask me to do something for you! Pro tip: you can upload a file and I'll be able to use it.",
@@ -52,7 +54,7 @@ function App() {
       },
     ])
   );
-  let [waitingForSystem, setWaitingForSystem] = useState<WaitingStates>(
+  const [waitingForSystem, setWaitingForSystem] = useState<WaitingStates>(
     WaitingStates.Idle
   );
   const chatScrollRef = React.useRef<HTMLDivElement>(null);
@@ -77,7 +79,11 @@ function App() {
 
   const handleCommand = (command: string) => {
     if (command == "reset") {
-      addMessage({ text: "Restarting the kernel.", type: "message", role: "system" });
+      addMessage({
+        text: "Restarting the kernel.",
+        type: "message",
+        role: "system",
+      });
 
       fetch(`${Config.API_ADDRESS}/restart`, {
         method: "POST",
@@ -120,14 +126,15 @@ function App() {
       const data = await response.json();
       const code = data.code;
 
-      addMessage({ text: data.text, type: "message", role: "generator" });
+      // Don't output the code gen text
+      // addMessage({ text: data.text, type: "message", role: "generator" });
 
       if (response.status != 200) {
         setWaitingForSystem(WaitingStates.Idle);
         return;
       }
-      
-      if (!!code) {
+
+      if (code) {
         submitCode(code);
         setWaitingForSystem(WaitingStates.RunningCode);
       } else {
@@ -142,13 +149,13 @@ function App() {
   };
 
   async function getApiData() {
-    if(document.hidden){
+    if (document.hidden) {
       return;
     }
-    
-    let response = await fetch(`${Config.API_ADDRESS}/api`);
-    let data = await response.json();
-    data.results.forEach(function (result: {value: string, type: string}) {
+
+    const response = await fetch(`${Config.API_ADDRESS}/api`);
+    const data = await response.json();
+    data.results.forEach(function (result: { value: string; type: string }) {
       if (result.value.trim().length == 0) {
         return;
       }
@@ -190,34 +197,33 @@ function App() {
     chatScrollRef.current!.scrollTop = chatScrollRef.current!.scrollHeight;
   }, [chatScrollRef, messages]);
 
-
   // Capture <a> clicks for download links
   React.useEffect(() => {
     const clickHandler = (event: any) => {
-      let element = event.target;
-      
+      const element = event.target;
+
       // If an <a> element was found, prevent default action and do something else
-      if (element != null && element.tagName === 'A') {
+      if (element != null && element.tagName === "A") {
         // Check if href starts with /download
-        
+
         if (element.getAttribute("href").startsWith(`/download`)) {
           event.preventDefault();
 
           // Make request to ${Config.WEB_ADDRESS}/download instead
           // make it by opening a new tab
           window.open(`${Config.WEB_ADDRESS}${element.getAttribute("href")}`);
-        }        
+        }
       }
     };
 
     // Add the click event listener to the document
-    document.addEventListener('click', clickHandler);
+    document.addEventListener("click", clickHandler);
 
     // Cleanup function to remove the event listener when the component unmounts
     return () => {
-      document.removeEventListener('click', clickHandler);
+      document.removeEventListener("click", clickHandler);
     };
-  }, []); 
+  }, []);
 
   return (
     <>
